4c03514556de0a3bae9cf1b197c4e20ef056ef5e
mw <maartenwierda@gmail.com>
2012-12-13 21:15:55 +0100

    Bookshizzle.

--- index.php
+++ index.php
@@ -4,43 +4,19 @@ require 'vendor/autoload.php';
 
 class MustacheView extends \Slim\View
 {
-    /**
-     * @var string The path to the directory containing Mustache.php
-     */
-    public static $mustacheDirectory = null;
-
-    /**
-     * Renders a template using Mustache.php.
-     *
-     * @see View::render()
-     * @param string $template The template name specified in Slim::render()
-     * @return string
-     */
-    public function render($template)
-    {
-        require_once self::$mustacheDirectory . '/Autoloader.php';
-        \Mustache_Autoloader::register(dirname(self::$mustacheDirectory));
-			$m = new \Mustache_Engine;
-			$m->setPartialsLoader(new \Mustache_Loader_FilesystemLoader('templates/partials'));
-			$m->addHelper('_i18n', function($text) {
-				// IRL, you would use something far more robust :)
-				$dictionary = array(
-					'Hello.' => 'Hola.',
-					'My name is {{ name }}.' => 'Me llamo {{ name }}.'
-				);
-				return array_key_exists($text, $dictionary) ? $dictionary[$text] : $text;
-			});
-			$m->addHelper('test', function($text) {
-				return 'test!';
-			});
-			
-			
-        $contents = file_get_contents($this->getTemplatesDirectory() . '/' . ltrim($template, '/'));
-        return $m->render($contents, $this->data);
+	public function render($template)
+	{
+		$m = new \Mustache_Engine;
+		$m->setPartialsLoader(new \Mustache_Loader_FilesystemLoader('templates'));
+		$m->addHelper('test', function($text) {
+			return 'test!';
+		});
+					
+		$contents = file_get_contents($this->getTemplatesDirectory() . '/' . ltrim($template, '/'));
+		return $m->render($contents, $this->data);
     }
 }
 
-MustacheView::$mustacheDirectory = 'vendor/mustache/mustache/src/Mustache';
 if ($_SERVER['SERVER_NAME'] == 'dev4.mediamatic.nl') {
 	ORM::configure('mysql:host=192.168.1.99;dbname=emwee');
 	ORM::configure('username', 'root');
@@ -52,9 +28,6 @@ else {
 	ORM::configure('password', 'root');
 }
 
-class Book extends Model
-{
-}
 /*
 
 echo '<pre>';
@@ -98,50 +71,41 @@ $app->notFound(function () use ($app) {
     echo '404';
 });
 
-class ViewModel {
-	
-	public $name = 'Maarten';
-	public $date = 'Maarten';
-	
-	public function name() {
-		return 'MaartenWierda';
+class Book extends Model
+{
+	public function index() {
+		$books = array_map(function ($book) {
+		    return $book->as_array();
+		}, Model::factory('Book')->find_many());
+		
+		return $books;
 	}
 	
-	public function setDate($date) {
-		$this->date = $date;
+	public function get($id) {
+		return Model::factory('Book')->find_one($id)->as_array();
 	}
 }
 
-$app->get('/test', function () use ($app) {
-	$bla = new \ViewModel;
-	$app->render('hello.mustache', array('data' => $bla));
-});
-
-$app->get('/hello/:name+/archive(/:year(/:month(/:day)))', function ($name, $year = 2010, $month = 12, $day = 05) use ($app) {
+$app->get('/books', function () use ($app) {
 	
-	$bla = new \ViewModel;
-	$bla->setDate(sprintf('%s-%s-%s', $year, $month, $day));
-	
-	if ( is_array($name) && $name[0] === 'Waldo' ) {
-		$app->notFound();
-	} else {
-		//$app->render('hello.mustache', array( 'name' => $name ));
-		
-		$view = $app->view();
-		$view->setData(array(
-		    'color' => 'red',
-		    'size' => 'medium'
-		));
-		
-		$view->appendData(array(
-		    'size' => 'large'
-		));
-		
-		$app->render('hello.mustache', array('data' => $bla ));
+	$books = Book::index();
+	foreach ($books as &$book) {
+		$book['url'] = $app->urlFor('book_view', array('id' => $book['id']));
 	}
-})->name('hello');
+	
+	$app->render('books/index.mustache', array(
+		'books' => $books,
+		'books_count' => count($books)
+	));
+	
+})->name('books_index');
 
-/* var_dump($app->urlFor('hello', array('name' => 'Joe/Hoe', 'year' => '2006'))); */
+$app->get('/book/:id', function ($id) use ($app) {
+	
+	$book = Book::get($id);
+	
+	$app->render('books/view.mustache', $book);
+})->name('book_view');
 
 $app->run();
 
